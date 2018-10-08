e684306993e00b4b5e5c684273ea4909923c0a4a
Driga Remus <drigaremuscristian@gmail.com>
2018-10-08 15:06:39 +0300

    Primul

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld
+{
+  public static void main(String args[])
+    {
+        
+         for (int i=0;i<=5;i++)
+        
+      { System.out.println("Test"+i);
+       System.out.println("Rulat"+i);
+       System.out.println("Cu"+i);
+       System.out.println("Succes"+i);
+       System.out.println("Succes"+i);
+       System.out.println("penis"+i);
+      }
+    }
+}
