62ffa28e3740ce85c7e1cceff05c9fa80873547e
RemusCristian96 <38312045+RemusCristian96@users.noreply.github.com>
2020-03-06 15:48:01 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,7 @@ public class HelloWorld
   public static void main(String args[])
     {
         
-         for (int i=0;i<=5;i++)
-        
-      { System.out.println("Test"+i);
-       System.out.println("Rulat"+i);
-       System.out.println("Cu"+i);
-       System.out.println("Succes"+i);
-       System.out.println("Succes"+i);
-       System.out.println("penis"+i
-      }
+      System.out.Println("Test"); 
+      
     }
 }
